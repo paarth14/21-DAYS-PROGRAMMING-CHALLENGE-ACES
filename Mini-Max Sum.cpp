4d76870a3105efb75c3e1da5aa692d5ba4df3ad7
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2020-10-06 22:44:03 +0530

    Day 2

---
+++ Mini-Max Sum.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int arr[5];
+
+    for(int i=0; i<5; i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+5);
+
+    long long int sum=0;
+    for(int i=0; i<4; i++)
+    {
+        sum += arr[i];
+        
+    }
+    cout<<sum<<" ";
+    sum =0;
+    
+    for(int i=1; i<5; i++)
+    {
+        sum += arr[i];
+    }
+    cout<<sum;
+}
+
