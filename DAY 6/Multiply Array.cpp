f2c8823e7724a5eddff46ae239cae8ce2e51b44a
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2020-10-10 22:26:15 +0530

    Create Multiply Array.cpp

---
+++ DAY 6/Multiply Array.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int arr[n], product=1;
+	    
+	    for(int i=0; i<n; i++)
+	    {
+	        cin>>arr[i];
+	        product *= arr[i];
+	    }
+	    cout<<product<<endl;
+	}
+  
+  return 0;
+}
