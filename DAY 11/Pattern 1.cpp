67b340c7f15adfd471995403f0d01a382513a867
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2020-10-15 22:43:53 +0530

    Create Pattern 1.cpp

---
+++ DAY 11/Pattern 1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void halfPyramidPattern1(int n)
+{
+    for(int i=1; i<=n; i++) //outer loop
+    {
+        for(int j=1; j<=i; j++) //inner loop
+        {
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter the number of levels of pattern: "<<endl;
+    cin>>num;
+    
+    halfPyramidPattern1(num); //function calling
+    
+    return 0;
+}
+
+
+/*OUTPUT :-
+*                                                                                                                               
+**                                                                                                                              
+***                                                                                                                             
+****                                                                                                                            
+*****                                                                                                                           
+******
+/*
